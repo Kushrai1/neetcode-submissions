62cdb04877a6f7016d6a2affb5a1b815dfd253d3
Kush Rai <raisahab8889@gmail.com>
2026-08-01 21:53:33 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int j = s.length() - 1;
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            j--;
+        }
+
+        return true;
+    }
+}
